b8d1d8e40769479bdce0dce7510b286e444d20bd
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-08-15 10:21:53 +0530

    sign up set up done

--- blog-react-frontend/src/App.js
+++ blog-react-frontend/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {Routehandler} from './router/routerhandler'
+import "react-notifications/lib/notifications.css";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import { Button } from '@mui/material';
+
 
 function App() {
+
   return (
     <div className="App"> 
+    <NotificationContainer/>
     <Routehandler/>
     </div>
   );
--- blog-react-frontend/src/components/signup.jsx
+++ blog-react-frontend/src/components/signup.jsx
@@ -8,6 +8,9 @@ import FormLabel from '@mui/material/FormLabel'
 import FormHelperText from '@mui/material/FormHelperText'
 import { Button, Input, InputLabel } from '@mui/material'
 import { sendOtp, verifyOtp } from '../apis/auth'
+import {
+  NotificationManager,
+} from "react-notifications";
 
 
 
@@ -15,30 +18,53 @@ import { sendOtp, verifyOtp } from '../apis/auth'
 export function Signup() {
     const [fields, setFields] = useState({})
     const [email, setEmail] = useState({})
+    const [emailError, setEmailError] = useState(false)
     const [otpSent, setOtpSent] = useState(false)
 
+    function ValidateEmail(email) {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return true;
+      }
+      return false;
+    }
     const handleChange = (event) => {
+      
         setFields({
             ...fields,
             [event.target.name]: event.target.value
         })
 
         if(event.target.name == 'email'){
+          setEmailError(false)
             setEmail({
               email: event.target.value,
             });
-        }
+          }
+        
     }
+    const sendNotificationSuccess = (message) => {
+      NotificationManager.success(message, "Sucess");
+    };
+     const sendNotificationFailure = (message) => {
+       NotificationManager.error(message, "Sucess");
+     };
+
 
     const handleOtp =async () =>{
-        try{
-            const res = await sendOtp(email)
-            alert(res?.data?.message)
-            setOtpSent(true)
+      if (ValidateEmail(email.email)) {
+        setEmailError(false);
+        try {
+          const res = await sendOtp(email);
+          alert(res?.data?.message);
+          setOtpSent(true);
+          sendNotificationSuccess(res?.data?.message);
+           
+        } catch (err) {
+          sendNotificationFailure("Please try again");
         }
-        catch(err){
-            console.log(err)
-        }
+      } else {
+        setEmailError(true);
+      }
 
     }
 
@@ -48,9 +74,8 @@ export function Signup() {
             console.log(res?.data)
         }
         catch(er){
-
+          console.log(er)
         }
-
     }
   return (
     <div>
@@ -60,75 +85,78 @@ export function Signup() {
         </Toolbar>
       </AppBar>
       <FormControl>
-        {!otpSent ?
-        <>
-        <TextField
-          id=""
-          label="First Name"
-          type="text"
-          name="first_name"
-          error={false}
-          margin="normal"
-          onChange={handleChange}
-        />
-        <FormHelperText id="my-helper-text">
-          We'll never share your email.
-        </FormHelperText>
-        <TextField
-          id=""
-          label="Last Name"
-          name="last_name"
-          margin="normal"
-          onChange={handleChange}
-        />
-        <TextField
-          id=""
-          label="Email"
-          name="email"
-          margin="normal"
-          onChange={handleChange}
-        />
-        <TextField
-          id=""
-          label="Password"
-          type="password"
-          name="password"
-          margin="normal"
-          onChange={handleChange}
-        />
-        <TextField id="" label="Confirm Password" margin="normal" />
-        <TextField
-          id=""
-          label="Mobile"
-          type="mobile"
-          name="mobile"
-          margin="normal"
-          onChange={handleChange}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          margin="normal"
-          onClick={handleOtp}
-        >
-          Send OTP
-        </Button>
-        </>
-        :
-        <>
-        <TextField
-          id=""
-          label="OTP"
-          type="mobile"
-          name="otp"
-          margin="normal"
-          onChange={handleChange}
-        />
-         <Button  variant="contained" color="primary" onClick={handleSubmit} >
-          Login
-        </Button> 
+        {!otpSent ? (
+          <>
+            <TextField
+              id=""
+              label="First Name"
+              type="text"
+              name="first_name"
+              margin="normal"
+              onChange={handleChange}
+            />
+            <TextField
+              id=""
+              label="Last Name"
+              name="last_name"
+              margin="normal"
+              onChange={handleChange}
+            />
+            <TextField
+              id=""
+              label="Email"
+              name="email"
+              error={emailError}
+              margin="normal"
+              onChange={handleChange}
+            />
+            {emailError && (
+              <FormHelperText id="my-helper-text">
+                Invalid email address.
+              </FormHelperText>
+            )}
+            <TextField
+              id=""
+              label="Password"
+              type="password"
+              name="password"
+              margin="normal"
+              onChange={handleChange}
+            />
+            <TextField id="" label="Confirm Password" margin="normal" />
+            <TextField
+              id=""
+              label="Mobile"
+              type="mobile"
+              name="mobile"
+              margin="normal"
+              onChange={handleChange}
+            />
+            <Button
+              loadingPosition="start"
+              variant="contained"
+              color="primary"
+              margin="normal"
+              onClick={handleOtp}
+            >
+              Send OTP
+            </Button>
           </>
-}
+        ) : (
+          <>
+            <TextField
+              id=""
+              label="OTP"
+              type="mobile"
+              name="otp"
+              margin="normal"
+              onChange={handleChange}
+            />
+            <Button variant="contained" color="primary" onClick={handleSubmit}>
+              Login
+            </Button>
+          </>
+        )}
       </FormControl>
     </div>
   );
